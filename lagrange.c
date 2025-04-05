ab7810d9c561cc6cbd2457a82bc3717237018f31
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-05 00:31:28 -0300

    :sparkles: start lagrange implementation

---
+++ lagrange.c
@@ -0,0 +1,19 @@
+// Algoritmo de Lagrange implementado
+//
+// Pedro Salviano Santos - 236586
+// Luiz Gustavo Motta Viana - 236428
+// Erick Ferreira Ribeiro - 237046
+
+// Esse código implementa o algoritmo estatístico da "Lagrange"
+// Foram utilizados ponteiros e alocação dinâmica de memória
+// Busca-se utilizar boas práticas de memória e clang-tidy
+
+// Estrutura criada para suportar o conjunto de pontos conhecidos da função
+struct knownPoints {
+    float x;
+    float y;
+};
+
+void main() {
+
+}
