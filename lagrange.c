94a56597677aa9cb6a5ac6c6d364277a55caa5b4
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-05 01:06:11 -0300

    :sparkles: add x-value getting function

--- lagrange.c
+++ lagrange.c
@@ -20,19 +20,38 @@ typedef struct {
 
 void alocKnownPoints(knownPoints **ptr, int size);
 void alocInts(int **ptr, int size);
-void storeDegree(int *p);
+void alocFloats(float **ptr, int size);
+void storeDegree(int *ptr);
+void storeXValue(float *ptr);
 
 void main() {
     knownPoints *ptrKnowPoints = NULL;
     int *polynomialDegree = NULL;
     int knowPointsAmount = 0;
+    float *xValue = NULL;
 
+    // O primeiro passo que é executado aqui é a determinação do grau do polinômio que deve ser calculado
+    // A primeira função aloca o espaço de memória que será utilizado pelo ponteiro
     alocInts(&polynomialDegree, 1);
-
+    // Armazena no ponteiro e na variável o valor do grau do polinômio e quantidade de pontos
     storeDegree(polynomialDegree);
     knowPointsAmount = *polynomialDegree + 1;
 
+    // O segundo passo é armazenar em um ponteiro (xValue) o valor de X que se quer obter através do polinômio
+    alocFloats(&xValue, knowPointsAmount);
+    // Armazena no ponteiro o último valor de X que o usuário digitou
+    storeXValue(xValue);
+
+    // Para o terceiro passo é necessário alocar e armazenar em um ponteiro os valores conhecidos/tabelados
+    // Para isso utilizamos um struct e um ponteiro dessa struct
+    alocKnownPoints(&ptrKnowPoints, knowPointsAmount);
+    // Armazena no ponteiro os valores tabelados
+    storeKnownPoints(ptrKnowPoints, knowPointsAmount);
+
     printf("\nQuantidade de pontos conhecidos: %d", knowPointsAmount);
+    printf("\nValor de X alvo: %.2f", *xValue);
+
+    exit(0);
 }
 
 // Função para alocar espaços de memória para o tipo das struct knowPoints
@@ -51,8 +70,26 @@ void alocInts(int **ptr, const int size) {
     }
 }
 
+// Função genérica para alocar espaços de memória para o tipo float
+void alocFloats(float **ptr, const int size) {
+    if ((*ptr = (float*) realloc(*ptr, size*sizeof(float)))==NULL) {
+        printf("\nErro de alocacao.");
+        exit(0);
+    }
+}
+
 // Função utilizada para armazenar em um ponteiro um valor que indica o grau do polinômio que será calculado
-void storeDegree(int *p) {
+void storeDegree(int *ptr) {
     printf("\nDigite o grau do polinomio: ");
-    scanf("%d", p);
+    scanf("%d", ptr);
+}
+
+// Função utilizada para armazenar em um ponteiro o valor que indica para qual X deseja-se obter f(X) pelo polinomio
+void storeXValue(float *ptr) {
+    printf("\nDigite o valor de X desconhecido que se quer obter com o polinomio: ");
+    scanf("%f", ptr);
+}
+
+void storeKnownPoints() {
+    
 }
