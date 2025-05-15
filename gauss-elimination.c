92766afe7fc3f97bdfb0c5c782dc385690eebaa4
Pedro S Santos <pedrosalviano170@gmail.com>
2025-05-15 02:53:43 -0300

    :sparkles::boom: add gauss elimination logic

--- gauss-elimination.c
+++ gauss-elimination.c
@@ -27,6 +27,7 @@ void allocMatrix(double ***matrix, int n);
 void printEquationSystem(double **matrix, int n);
 void printMatrix(double **matrix, int n);
 void readUserInput(double **matrix, int n);
+void gaussElimination(double **matrix, int n);
 
 int main(void) {
     const int n = getNvalue();
@@ -36,6 +37,10 @@ int main(void) {
     readUserInput(matrix, n);
 
     printf("\nMatriz aumentada:\n\n");
+    printMatrix(matrix, n);
+
+    gaussElimination(matrix, n);
+    printf("\nMatriz depois da eliminacao de gauss:\n");
     printMatrix(matrix, n);
 
     return EXIT_SUCCESS;
@@ -71,12 +76,12 @@ void printEquationSystem(double **matrix, const int n) {
     for (int i = 0; i < n; i++) {
         printf("{ ");
         for (int j = 0; j < n; j++) {
-            printf("(%6.2lf * x%d)", matrix[i][j], j + 1);
+            printf("(%6.3lf * x%d)", matrix[i][j], j + 1);
             if (j < n - 1) {
                 printf(" + ");
             }
         }
-        printf(" = %6.2lf", matrix[i][n]); // termo independente
+        printf(" = %6.3lf", matrix[i][n]); // termo independente
         printf(" }\n");
     }
 }
@@ -85,9 +90,9 @@ void printMatrix(double **matrix, const int n) {
     for (int i = 0; i < n; i++) {
         printf("| ");
         for (int j = 0; j < n; j++) {
-            printf("%6.2lf ", matrix[i][j]);
+            printf("%6.3lf ", matrix[i][j]);
         }
-        printf("| %6.2lf |\n", matrix[i][n]);
+        printf("| %6.3lf |\n", matrix[i][n]);
     }
     printf("\n");
 }
@@ -98,10 +103,15 @@ void readUserInput(double **matrix, const int n) {
     for (int i = 0; i < n; i++) {
         printf("\nEquação %d:\n", i + 1);
 
-        for (int j = 0; j < n+1; j++) {
+        for (int j = 0; j < n + 1; j++) {
+            char label[50];
             printEquationSystem(matrix, n);
-            // Exibe a descrição de maneira mais compacta
-            printf("\n  %s%d: ", (j == n - 1) ? "b_" : "Coeficiente de x_", j + 1);
+
+            // Usa ternário com snprintf para gerar a string label
+            j == n ? snprintf(label, sizeof(label), "b_%d", i + 1)
+                     : snprintf(label, sizeof(label), "Coeficiente de x_%d", j + 1);
+
+            printf("\n  %s: ", label);
             scanf("%lf", &matrix[i][j]);
         }
     }
@@ -109,22 +119,40 @@ void readUserInput(double **matrix, const int n) {
 
 void gaussElimination(double **matrix, const int n) {
     for (int k = 1; k < n; k++) {
-        printf("\nPasso %d da eliminação de gauss...", k);
+        printf("\n\n### Passo %d da eliminação de gauss ###\n", k);
         const int superiorLimit = n;
         const int inferiorLimit = k + 1;
+        // — 1 devido à indexação de vetores/matrizes começar em 0
         const double pivotElement = matrix[k-1][k-1];
         printf(
-            "\ni >= %d | i <= %d | pivô (a_kk) = (a_%d%d) = %lf",
+            "\nk = %d | i >= %d | i <= %d | pivô (a_kk) = (a_%d%d) = %lf\n",
+            k,
             inferiorLimit,
             superiorLimit,
             k,
             k,
             pivotElement
         );
+        // A variável 'i' nesse caso indica a linha que está sendo acessada
         for (int i = inferiorLimit; i <= superiorLimit; i++) {
-            const double lineMultiplier = matrix[i][k] / pivotElement;
-            for (int j = k; j <= n; j++) {
-                matrix[k][j] = matrix[k][j] - lineMultiplier * pivotElement;
+            // — 1 devido à indexação de vetores/matrizes começar em 0
+            const double lineMultiplier = matrix[i-1][k-1] / pivotElement;
+            printf("\nm_%d%d = %.3lf\n", i, k, lineMultiplier);
+            // A variável 'j' nesse caso indica a coluna que está sendo acessada
+            // O +1 na variável 'j' representa a indicação de que temos um termo independente na matriz aumentada
+            for (int j = k; j <= n + 1; j++) {
+                const double oldValue = matrix[i-1][j-1];
+                const double newValue = oldValue - lineMultiplier * matrix[k-1][j-1];
+
+                // Usa ternário com snprintf para gerar a string label
+                char label[64];
+                j == n + 1 ? snprintf(label, sizeof(label), "b_%d = b_%d - (m_%d%d * b_%d) = ", i, i, i, k, k)
+                         : snprintf(label, sizeof(label), "a_%d%d = a_%d%d - (m_%d%d * a_%d%d) = ", i, j, i, j, i, k, k, j);
+                printf("%s", label);
+
+                printf("%.3lf - (%.3lf * %.3lf) = %.3lf\n", oldValue, lineMultiplier, matrix[k-1][j-1], newValue);
+
+                matrix[i-1][j-1] = newValue;
             }
         }
     }
